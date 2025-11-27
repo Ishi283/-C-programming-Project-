53a8fbbe7a89be004e8a770b166aaf0b52a9acb3
Ishi283 <ishiagarwal2829@gmail.com>
2025-11-28 01:30:39 +0530

    Add files via upload

--- project- Hospital management system.c
+++ project- Hospital management system.c
@@ -8,6 +8,12 @@ struct details
     int age;
     char disease[50];
 };
+struct doctor
+{
+    int id;
+    char name[50];
+    char dept[100];
+};
 int count =0;
 void add( struct details d[])
 {
@@ -47,7 +53,7 @@ void search(struct details d[])
 {
     int id,found=0;
     printf("\nEnter patient id to search:");
-    sacnf("%d",&id);
+    scanf("%d",&id);
     for(int i=0; i<count; i++)
     {
         if(d[i].id == id)
@@ -67,4 +73,61 @@ void search(struct details d[])
         printf("Patient not found");
     }
 }
+void delete(struct patient d[])
+{
+    int id,found=0;
+    printf("Enter Patient id:");
+    scanf("%d",&id);
+    for(int i=0; i<count; i++)
+    {
+        if(d[i].id == id)
+        {
+            found = 1;
+            for(int j; j<count-1; j++)
+            {
+            d[j]= d[j+1];
+            }
+        count--;
+        printf("Patient deleted");
+        break;
+        }
+    }
+    if(!found)
+    {
+        printf("Patient not found");
+    }
+}
+int dcount=0;
+void adddoc(struct doctor doc[])
+{
+    printf("\n--------Add Doctor-------\n");
+    printf("Enter doctor id:");
+    scanf("%d",&doc[].id);
+    printf("Enter doctor name:");
+    scanf("%s",doc[].name);
+    printf("Enter department:");
+    scanf("%s",doc[].dept);
+    dcount++;
+    printf("Doctor added successfully\n");
+}
+void view(struct doctor doc[])
+{
+    printf("\n------Doctor list--------\n");
+    if(dcount==0)
+    {
+        printf("No doctor found\n");
+        return;
+    }
+    else
+    {
+        for(int i=0; i<dcount; i++)
+        {
+            printf("\nDoctor %d\n",i+1);
+            printf("ID:%d\n",doc[i].id);
+            printf("Name:%s\n",doc[i].name);
+            printf("Department:%s\n",doc[i].dept);
+        }
+    }
+}
+
 
